2cb1b80c28ae47ac4a75906045f1e167c995be08
claudiaperezdiaz <claupedi1307@gmail.com>
2026-03-22 21:26:11 +0100

    Update GildedRose.java

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -12,9 +12,14 @@ class GildedRose {
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
+    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+        items[i].quality = items[i].quality - 1;
+
+        if (items[i].name.equals("Conjured") && items[i].quality > 0) {
+            items[i].quality = items[i].quality - 1;
+        }
+    }
+}
                 }
             } else {
                 if (items[i].quality < 50) {
@@ -44,10 +49,14 @@ class GildedRose {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
+    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+        items[i].quality = items[i].quality - 1;
+
+        if (items[i].name.equals("Conjured") && items[i].quality > 0) {
+            items[i].quality = items[i].quality - 1;
+        }
+    }
+}
                     } else {
                         items[i].quality = items[i].quality - items[i].quality;
                     }
